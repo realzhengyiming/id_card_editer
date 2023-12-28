14be7d3cbb5dd51ce10087dbf5ab6ebd48fac3c5
zhengyiming <realzhengyiming@outlook.com>
2023-12-28 16:36:01 +0800

    finishing html and server base function(upload config and batch process)

---
+++ batch_process_util.py
@@ -0,0 +1,150 @@
+import base64
+import io
+import json
+from typing import Dict
+
+
+def read_config_json(json_path) -> Dict:
+    with open(json_path) as file:
+        config_dict = json.loads(file.read())
+    return config_dict
+
+
+def read_mapping_excel(excel_path):
+    import pandas as pd
+    df = pd.read_excel(excel_path)
+    return df
+
+
+from PIL import ImageDraw, ImageFont
+from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
+import os
+
+font_base_path = "/Users/zhengyiming/PycharmProjects/id_card_editer/static/fonts/"
+
+
+def draw_text(image, fontname, fontsize, fontpose, fontcolor, content, font_base_path):  # fontpose(x,y)
+    print(fontname, fontsize, fontpose, fontcolor, content)
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype(os.path.join(font_base_path, fontname), fontsize)
+    # 在指定位置添加文字
+    draw.text(fontpose, content, fill=fontcolor, font=font, antialias=True)  # 在坐标 (50, 50) 处添加文字
+
+
+def cut_avatar(image, size, is_radias=False):
+    image = image.convert("RGBA")
+    # 使用ImageOps模块的fit方法将图片缩放并居中裁剪，以适应元素大小
+    result = ImageOps.fit(image, size, centering=(0.5, 0.5), method=Image.Resampling.BICUBIC)
+    if is_radias:
+        mask = Image.new("L", result.size, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0) + result.size, fill=255)
+        # 将遮罩应用到结果图片上，并保存
+        result.putalpha(mask)
+    return result
+
+
+def paste_with_transparency(background, overlay, position):
+    # 打开底图和叠加图
+    # 确保图像都是 RGBA 模式，以支持透明度
+    background = background.convert("RGBA")
+    overlay = overlay.convert("RGBA")
+    # 将叠加图粘贴到底图上
+    background.paste(overlay, position, overlay)
+    return background
+
+
+def pil_read_base64(string):
+    # 假设这是你的 base64 编码的图片数据
+    # 移除 base64 编码字符串中的前缀部分（data:image/jpeg;base64, 等）
+    img_data = base64.b64decode(string.split(',')[1])
+    # 将解码后的数据转换为字节流
+    image_stream = io.BytesIO(img_data)
+    image = Image.open(image_stream)
+    return image
+
+
+def render_image_by_config(df, config_dict, font_base_path, avatar_dir) -> Dict:
+    base64_data = config_dict['background']['imageUrl']
+    backgroup_img = pil_read_base64(base64_data)
+    x, y = config_dict['background']['size']['width'], config_dict['background']['size']['height']
+
+    background_resized_raw = backgroup_img.resize((x, y))
+
+    # 创建一个可绘制对象
+    templated = [i for i in config_dict['textList'] if i.get("isTemplate")]
+    untemplated = [i for i in config_dict['textList'] if i.get("isTemplate") is False]
+
+    # 读取头像数据模版配
+    image_filenames = [i for i in os.listdir(avatar_dir) if i.split(".")[-1] in ("png", "jpeg", "jpg", "gif")]
+    image_dict = {i.split(".")[0]: i for i in image_filenames}  # 减少便利次数
+
+    # 按行绘制模版填充的内容
+    all_images = {}
+    for index, row in df.iterrows():
+        row_dict = dict(row)
+        background_resized = background_resized_raw.copy()
+        # 读取图片
+        avatar_dict = config_dict['avatar']
+        avatar_filename = str(row_dict[avatar_dict['colName']])
+        avatar_filename = image_dict[avatar_filename]
+
+        avatar_image = Image.open(os.path.join(avatar_dir, avatar_filename))
+        is_radias = avatar_dict['rounded']
+        x, y = avatar_dict['position']['left'], avatar_dict['position']['top']
+        avatar_position = (x, y)
+        width = int(avatar_dict['size']['width'].strip("px"))
+        height = int(avatar_dict['size']['height'].strip("px"))
+        avatar_cut = cut_avatar(avatar_image, (width, height), is_radias=is_radias)
+
+        background_resized = paste_with_transparency(background_resized, avatar_cut, avatar_position)
+
+        for text_data in config_dict['textList']:
+            is_template = text_data['isTemplate']
+            col_name = text_data['colName']
+            content = str(row_dict[col_name]) if is_template else text_data['content']  # 替换内容
+            fontcolor = text_data['color']
+            fontcolor = eval(fontcolor.replace("rgb", ""))
+
+            fontsize = int(text_data['fontSize'].replace("px", ""))
+            fontpose = (text_data['position']['left'], text_data['position']['top'])
+            fontname = text_data['fontFamily']
+
+            fontname = fontname[0:-4] + "." + fontname[-3:-1] + fontname[-1]
+            draw_text(background_resized, fontname, fontsize, fontpose, fontcolor, content, font_base_path)
+        all_images[avatar_filename] = background_resized
+        # 保存修改后的图片
+    # image.save("output.jpg")
+    return all_images
+
+
+def remove_folder(path):
+    # 确定路径是否存在
+    if os.path.exists(path):
+        # 遍历文件夹中的所有文件和子文件夹
+        for root, dirs, files in os.walk(path, topdown=False):
+            # 删除文件
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                os.remove(file_path)
+            # 删除子文件夹
+            for dir_name in dirs:
+                dir_path = os.path.join(root, dir_name)
+                os.rmdir(dir_path)
+
+        # 删除根文件夹
+        os.rmdir(path)
+        print(f"文件夹 '{path}' 及其所有内容已成功删除。")
+    else:
+        print(f"文件夹 '{path}' 不存在。")
+
+if __name__ == '__main__':
+    font_base_path = "/Users/zhengyiming/PycharmProjects/id_card_editer/static/fonts"
+    avatar_dir = "/Users/zhengyiming/PycharmProjects/id_card_editer/static/avatar/2023-12-26 13:31:57"
+    images = render_image_by_config(df, config_dict, font_base_path, avatar_dir)
+    image
+
+    for i in images:
+        plt.imshow(i)
+        plt.show()
--- server.py
+++ server.py
@@ -1,14 +1,26 @@
+import json
 import os
+import zipfile
+from datetime import datetime
 
-from flask import Flask, send_from_directory, render_template, Markup
+import pandas as pd
+from flask import Flask, send_from_directory, render_template, request, jsonify, send_file
+from markupsafe import Markup
+
+from batch_process_util import render_image_by_config, remove_folder
 
 app = Flask(__name__)
 
 # 设置静态资源目录
+project_root = os.path.dirname(os.path.abspath(__file__))
+
 app.static_folder = 'static'
 app.template_folder = "./"
-root_fonts_folder = "fonts"
+root_fonts_folder = os.path.join(app.static_folder, "fonts")
+output_folder = "output"  # 项目目录下的output路径
 app.debug = True  # 启用调试模式和自动重载
+
+os.makedirs(output_folder, exist_ok=True)
 
 
 # 定义路由，用于处理静态文件请求
@@ -21,6 +33,78 @@ def serve_static(path):
 def homepage():
     template = html_load_font()
     return render_template('deit_id_card.html', font_load=Markup(template))
+
+
+@app.route('/upload_folder', methods=['POST'])
+def upload():
+    files = request.files.getlist('files[]')
+
+    if files:
+        save_folder = get_now_date()
+        full_folder_path = os.path.join(app.static_folder, "avatar", save_folder)
+        os.makedirs(full_folder_path, exist_ok=True)
+        static_path = os.path.join("static", "avatar", save_folder)
+        uploaded_files = []
+        for file in files:
+            filename = os.path.join(full_folder_path, file.filename)
+            file.save(filename)
+            dir_path = os.path.join("static", "avatar")
+            uploaded_files.append(dir_path)
+        return jsonify({'message': 'Files uploaded successfully',
+                        'storeFolder': static_path,
+                        "storeFiles": uploaded_files})
+    else:
+        return jsonify({'message': 'Files uploaded error, no files selected!',
+                        'storeFolder': "",
+                        "storeFiles": []})
+
+
+@app.route('/batch_processing', methods=['POST'])
+# 这个只是解析服务，然后批量生成还是交给js， 那就复杂起来； 一半python，一半js
+def batch_processing():
+    error1_response = jsonify({'message': 'error! static path is empty!'})
+    error2_response = jsonify({'message': "haven't uploaded config .json or .excel file!"})
+    data = request.json
+    static_path = data.get("static_path")
+    end_filename = static_path.split('/')[-1]
+    if static_path == "":
+        return error1_response
+    else:
+        all_files = os.listdir(static_path)
+        excel_files = [i for i in all_files if i.split(".")[-1] in ("csv", "xlsx")]
+        json_files = [i for i in all_files if i.split(".")[-1] in ("json")]
+        if len(excel_files) < 1 or len(json_files) < 1:
+            return error2_response
+
+        with open(os.path.join(static_path, json_files[0]), 'r') as file:
+            config_dict = json.load(file)
+
+        # 读取 Excel 文件
+        df = pd.read_excel(os.path.join(static_path, excel_files[0]))
+        avatar_dir = static_path
+        font_base_path = root_fonts_folder
+        images_dict = render_image_by_config(df, config_dict, font_base_path, avatar_dir)
+
+        output_full_folder = static_path.replace("static", output_folder)
+        os.makedirs(output_full_folder, exist_ok=True)
+        for filename, image in images_dict.items():
+            image.save(os.path.join(output_full_folder, filename + ".png"))
+
+        # 创建一个 ZIP 文件并将图片添加到其中
+        zip_path = os.path.join(app.static_folder, "output", f"{end_filename}.zip")
+        image_files = os.listdir(output_full_folder)
+        with zipfile.ZipFile(zip_path, 'w') as zip_file:
+            for image_file in image_files:
+                image_path = os.path.join(output_full_folder, image_file)
+                zip_file.write(image_path, image_file)
+
+        relative_path = os.path.join("static", "output", f"{end_filename}.zip")
+        # 返回 ZIP 文件给用户下载
+        print("打包好了压缩包，给用户自己去下载")
+        print("zip_path:", relative_path)
+        remove_folder(output_full_folder)
+        return jsonify({'message': 'success!',
+                        'outputPath': "/" + relative_path})
 
 
 # 加载本地的字体
@@ -40,13 +124,21 @@ def html_load_font():
         load_ttf_template = f"""<style>
           /* 定义字体 */
           @font-face {{
-            font-family: '{filename.split(".")[0]}';
+            font-family: '{filename.replace(".", "_")}';
             src: url('/static/fonts/{filename}') format('{type_mapping[end_type]}');
             font-weight: normal;
             font-style: normal;
           }}</style>"""
         total_template += load_ttf_template
     return total_template
+
+
+def get_now_date():
+    now = datetime.now()
+    formatted_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return formatted_date_time
+
+
 
 
 # 启动服务器
