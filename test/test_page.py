a370f9bcc9d3025adb9137cdf91ddd9f43e75aae
zhengyiming <realzhengyiming@outlook.com>
2024-01-01 09:24:17 +0800

    update code

---
+++ test/test_page.py
@@ -0,0 +1,14 @@
+# test_app.py
+import pytest
+from app import app
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+def test_hello_world(client):
+    response = client.get('/')
+    assert 200 == response.status_code
